9a13676f79ae33144f2320171c392329afc4ebfe
Taisia Bo <thaisbox@gmail.com>
2017-04-02 17:20:45 +0300

    contact routes were changed

--- mcrblg/config/routes.rb
+++ mcrblg/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'contacts/index'
 
-  resource :contacts, only: [:new, :create]
+  resource :contacts, only: [:new, :create], path_names: {:new => ''}
   resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
