db829b94c40955f903e16d9c4af95c081259f162
Taisia Bo <thaisbox@gmail.com>
2017-04-04 02:11:51 +0300

    sign in/out upgrading

--- mcrblg/config/routes.rb
+++ mcrblg/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: "home#index"
+  root to: "welcome#index"
 
-  get 'home/index'
+  get 'welcome/index'
 
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
