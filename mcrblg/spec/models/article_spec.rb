ab0622a98c12fd5d8005deac32be3d6239a237e0
Taisia Bo <thaisbox@gmail.com>
2017-04-05 00:22:42 +0300

    more tests

--- mcrblg/spec/models/article_spec.rb
+++ mcrblg/spec/models/article_spec.rb
@@ -1,5 +1,10 @@
 require "spec_helper"
 
 describe Article do
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:text) }
+  
   it { should have_many(:comments) }
+
 end
