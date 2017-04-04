fb2a8d9d707ce65ab13dafb921259d6625ea09f5
Taisia Bo <thaisbox@gmail.com>
2017-04-05 01:14:28 +0300

    factory girl installation and tests

--- mcrblg/spec/models/article_spec.rb
+++ mcrblg/spec/models/article_spec.rb
@@ -4,7 +4,12 @@ describe Article do
 
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:text) }
-  
+
   it { should have_many(:comments) }
+
+  it "brings title" do
+    article = create(:article, title: 'Ololo lolo')
+    expect(article.subject).to eq 'Ololo lolo'
+  end
 
 end
