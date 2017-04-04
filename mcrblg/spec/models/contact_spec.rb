a8a0fbb5871a79494c6fce853b79459d70291abc
Taisia Bo <thaisbox@gmail.com>
2017-04-05 00:09:03 +0300

    first tests

--- mcrblg/spec/models/contact_spec.rb
+++ mcrblg/spec/models/contact_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe Contact do
 
-  #it { should validate_presence_of(:email) }
   it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:message) }
 
 end
